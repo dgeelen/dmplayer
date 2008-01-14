243e7c0dd15a11c3131ae129f1ae1a9da6a160d2
daan.wissing <daan.wissing@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-14 12:29:41 +0000

    nice QT Gui

--- src/qt-client/MainWindow.h
+++ src/qt-client/MainWindow.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include "ui_MainWindow.h"
+#include <string>
+#include "..\audio\mp3\mp3_interface.h"
 
 class MainWindow: public QMainWindow, public Ui::MainWindow
 {
@@ -10,5 +12,15 @@ class MainWindow: public QMainWindow, public Ui::MainWindow
 	public:
 		MainWindow();
 		~MainWindow();
+	public slots:
+		void on_OpenButton_clicked();
+		void on_PreviousButton_clicked();
+		void on_PlayButton_clicked();
+		void on_PauseButton_clicked();
+		void on_StopButton_clicked();
+		void on_NextButton_clicked();
+	private:
+		std::string file;
+		mp3_handler* handler;
 };
 #endif
