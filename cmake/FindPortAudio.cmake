1efe7a5e7c54b3c255d45fb1231bfb81a7f14d62
daan.wissing <daan.wissing@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-17 12:00:04 +0000

    fixed cmake/FindPortAudio.cmake to find portaudio :)

---
+++ cmake/FindPortAudio.cmake
@@ -0,0 +1,20 @@
+Find_Path(PORTAUDIO_INCLUDE_DIR
+  portaudio.h
+  usr/include usr/local/include
+  "C:/Program Files/portaudio_v18_1/pa_common"
+  "D:/portaudio_v18_1/pa_common"
+  )
+  
+Find_Library(PORTAUDIO_LIBRARY
+  portaudio
+  usr/lib usr/local/lib
+  "C:/Program Files/portaudio_v18_1/lib"
+  "D:/portaudio_v18_1/lib"
+  )# Do something about the version of portaudio?
+  
+IF(PORTAUDIO_INCLUDE_DIR AND PORTAUDIO_LIBRARY )
+  SET(PORTAUDIO_FOUND TRUE)
+  IF(WIN32)
+    SET(PORTAUDIO_LIBRARY ${PORTAUDIO_LIBRARY} winmm)
+  ENDIF(WIN32)
+ENDIF(PORTAUDIO_INCLUDE_DIR AND PORTAUDIO_LIBRARY )
