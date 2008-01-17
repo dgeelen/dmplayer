b87e7ed04c8336d555a4dc0cadaa064654c16710
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-17 13:07:02 +0000

    add some status messages

--- src/audio/audio_controller.cpp
+++ src/audio/audio_controller.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include "audio_controller.h"
 #include "backend_interface.h"
+#include "../error-handling.h"
 #ifdef PORT_AUDIO_BACKEND
 	#include "backend_portaudio.h"
 #endif
@@ -14,12 +15,14 @@ AudioController::AudioController() : IDecoder() {
 	try {
 		PortAudioBackend* be = new PortAudioBackend(this);
 		backend = be;
+		dcerr("AudioController: using PortAudioBackend");
 	} catch(...) {}
 	#endif
 	#ifdef SDL_MIXER_BACKEND
 	try {
 		SDLMixerBackend* be = new SDLMixerBackend(this);
 		backend = be;
+		dcerr("AudioController: using SDLMixerBackend");
 	} catch(...) {}
 	#endif
 	if(backend == NULL) {
