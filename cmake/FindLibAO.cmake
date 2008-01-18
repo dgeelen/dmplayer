16e1a00629293accdb2dbe01c6ffad45d0d66b33
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-18 13:55:03 +0000

    forget something

---
+++ cmake/FindLibAO.cmake
@@ -0,0 +1,15 @@
+SET(LIBAO_FOUND FALSE)
+
+Find_Path(LIBAO_INCLUDE_DIR
+  ao.h
+  /usr/include/ao /usr/local/include/ao
+  )
+
+Find_Library(LIBAO_LIBRARY
+  ao
+  /usr/lib /usr/local/lib
+  )
+
+IF(LIBAO_INCLUDE_DIR AND LIBAO_LIBRARY)
+  SET(LIBAO_FOUND TRUE)
+ENDIF(LIBAO_INCLUDE_DIR AND LIBAO_LIBRARY)
