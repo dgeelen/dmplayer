980f0b4a74a28a793df8b7c2d714adce1f30595f
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-18 23:07:14 +0000

    zomg 1337 dcerr function :D

--- trunk/src/error-handling.h
+++ trunk/src/error-handling.h
@@ -4,7 +4,29 @@
 	#include <boost/function.hpp>
 	/* DEBUG #define's */
 	#ifdef DEBUG
-		#define dcerr(x) std::cerr << x << "\n"
+		#include <string>
+		#include <algorithm>
+		#include <sstream>
+		namespace DCERR {
+		static std::string file_basename(std::string s) {
+			int i = s.find_last_of("/", s.size());
+			s = s.substr( i + 1, s.size() - i - 1);
+			return s;
+		}
+		static std::stringstream ss;
+		}
+		#define dcerr_inttostr(x) ((DCERR::ss.str("")),(DCERR::ss<<x),(DCERR::ss.str()))
+		#define dcerr_filebasename (std::string(DCERR::file_basename(__FILE__)))
+		#define dcerr_filename_part (dcerr_filebasename + std::string( std::max(24 - (int)dcerr_filebasename.size(), 0), ' '))
+		#define dcerr_linenumber (dcerr_inttostr(__LINE__))
+		#define dcerr_linenumber_part ( std::string(std::max(5 - (int)dcerr_linenumber.size(), 0), '0') + dcerr_linenumber )
+		#define dcerr_function (std::string(__FUNCTION__))
+		#define dcerr_function_part (dcerr_function + "():" + std::string(std::max(24 - (int)dcerr_function.size(), 1), ' '))
+		#define dcerr(x) std::cerr << dcerr_filename_part \
+		                           << dcerr_linenumber_part << " " \
+		                           << dcerr_function_part \
+		                           << x \
+		                           << "\n"
 	#else
 		#define dcerr(x)
 	#endif
