f6ccb1336a2fbe5be3fea69d0cbb49f130cee8cb
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-19 20:30:16 +0000

    Trydecode for Raw Decoder.

--- src/audio/decoder_raw.cpp
+++ src/audio/decoder_raw.cpp
@@ -1,4 +1,5 @@
 #include "decoder_raw.h"
+#include <string>
 
 RawDecoder::RawDecoder()
 {
@@ -8,6 +9,7 @@ RawDecoder::RawDecoder()
 RawDecoder::RawDecoder(IDataSource* source)
 {
 	this->source = source;
+	//tryDecode(source);
 }
 
 RawDecoder::~RawDecoder()
@@ -16,7 +18,30 @@ RawDecoder::~RawDecoder()
 
 IDecoder* RawDecoder::tryDecode(IDataSource* datasource)
 {
-	return false; // everything is raw. but usually still not what we want
+	datasource->reset();
+	char hdr[48];
+	uint8* uhdr = (uint8*)hdr;
+	if (datasource->read(hdr, 48) != 48) return NULL;
+#define STRCHECK(x, str) for (unsigned int i = 0; i < strlen(str); ++i) if (hdr[x+i] != str[i]) return NULL;
+
+	STRCHECK( 0, "RIFF");
+	STRCHECK( 8, "WAVE");
+	STRCHECK(12, "fmt ");
+	STRCHECK(36, "data");
+
+	uint16 fmt      = uhdr[20] + (uhdr[21] << 8);
+	uint16 channels = uhdr[22] + (uhdr[23] << 8);
+	uint32 srate    = uhdr[24] + (uhdr[25] << 8) + (uhdr[26] << 16) + (uhdr[27] << 24);
+	uint16 bits     = uhdr[34] + (uhdr[35] << 8);
+
+	if (fmt != 1)       return NULL;
+	if (channels != 2)  return NULL; // TODO: mono->stereo filter?
+	if (srate != 22050) return NULL; // TODO: samplerate conversion filter?
+	if (bits != 16)     return NULL; // TODO: bps conversion filter?
+	// or, return a decoder with the info from the header, so the controller
+	// can change the backend output, or put in the conversion filters?
+
+	return new RawDecoder(datasource);
 }
 
 uint32 RawDecoder::doDecode(char* buf, uint32 max, uint32 req)
