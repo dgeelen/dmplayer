e14c372ccef071aa89e3c43fc5b45082e9802343
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-19 10:27:40 +0000

    Simplify error handling to actually compile.
    I hope it does more or less the same as it did before.

--- trunk/src/error-handling.h
+++ trunk/src/error-handling.h
@@ -5,28 +5,21 @@
 	/* DEBUG #define's */
 	#ifdef DEBUG
 		#include <string>
-		#include <algorithm>
-		#include <sstream>
+		#include <iomanip>
 		namespace DCERR {
-		static std::string file_basename(std::string s) {
-			int i = s.find_last_of("/", s.size());
-			s = s.substr( i + 1, s.size() - i - 1);
-			return s;
+			static std::string file_basename(std::string s) {
+				int i = s.find_last_of("/\\", s.size());
+				s = s.substr( i + 1, s.size() - i - 1);
+				return s;
+			}
 		}
-		static std::stringstream ss;
-		}
-		#define dcerr_inttostr(x) ((DCERR::ss.str("")),(DCERR::ss<<x),(DCERR::ss.str()))
-		#define dcerr_filebasename (std::string(DCERR::file_basename(__FILE__)))
-		#define dcerr_filename_part (dcerr_filebasename + std::string( std::max(28 - (int)dcerr_filebasename.size(), 0), ' '))
-		#define dcerr_linenumber (dcerr_inttostr(__LINE__))
-		#define dcerr_linenumber_part ( std::string(std::max(5 - (int)dcerr_linenumber.size(), 0), '0') + dcerr_linenumber )
-		#define dcerr_function (std::string(__FUNCTION__))
-		#define dcerr_function_part (dcerr_function + "():" + std::string(std::max(28 - (int)dcerr_function.size(), 1), ' '))
-		#define dcerr(x) std::cerr << dcerr_filename_part \
-		                           << dcerr_linenumber_part << " " \
-		                           << dcerr_function_part \
+		#define dcerr(x) std::cerr << std::setiosflags(std::ios::left) \
+		                           << std::setw(28) << DCERR::file_basename(__FILE__) \
+		                           << std::setw(5) << __LINE__ << ' ' \
+		                           << std::setw(28) << __FUNCTION__ << ' ' \
+		                           << std::setiosflags(std::ios::right) \
 		                           << x \
-		                           << "\n"
+		                           << '\n'
 	#else
 		#define dcerr(x)
 	#endif
