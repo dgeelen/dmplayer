0255cbe93fc83ca84adee05adc4a167a2337cfbc
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-19 18:11:26 +0000

    implement double buffering playback thread

--- trunk/src/audio/backend_libao.h
+++ trunk/src/audio/backend_libao.h
@@ -2,6 +2,9 @@
 #define BACKEND_LIBAO_H
 #include <ao.h>
 #include "backend_interface.h"
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <boost/thread/condition.hpp>
 
 class LibAOBackend : public IBackend {
 	public:
@@ -10,6 +13,19 @@ class LibAOBackend : public IBackend {
 	private:
 		ao_device *device;
 		ao_sample_format format;
+		int   playing_buffer;
+		char* audio_buffer[2];
+		int   audio_buffer_fill[2];
+		bool  play_back;
+
+		boost::mutex buffer_a_mutex;
+		boost::mutex buffer_b_mutex;
+		boost::condition buffer_a_condition;
+		boost::condition buffer_b_condition;
+		boost::thread* thread_decoder_read_thread;
+		boost::thread* thread_ao_play_thread;
+		void decoder_read_thread();
+		void ao_play_thread();
 };
 
 #endif//BACKEND_LIBAO_H
