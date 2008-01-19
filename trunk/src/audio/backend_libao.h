7cebae3d7108eaa1cd1836160a08e6da74612677
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-19 18:11:40 +0000

    use boost::barriers instead of boost::conditions

--- trunk/src/audio/backend_libao.h
+++ trunk/src/audio/backend_libao.h
@@ -5,12 +5,15 @@
 #include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition.hpp>
+#include <boost/thread/barrier.hpp>
 
 class LibAOBackend : public IBackend {
 	public:
 		LibAOBackend(IDecoder* dec);
 		virtual ~LibAOBackend();
 	private:
+		IDecoder* decoder;
+
 		ao_device *device;
 		ao_sample_format format;
 		int   playing_buffer;
@@ -18,10 +21,9 @@ class LibAOBackend : public IBackend {
 		int   audio_buffer_fill[2];
 		bool  play_back;
 
+		boost::barrier* fill_buffer_barrier;
 		boost::mutex buffer_a_mutex;
 		boost::mutex buffer_b_mutex;
-		boost::condition buffer_a_condition;
-		boost::condition buffer_b_condition;
 		boost::thread* thread_decoder_read_thread;
 		boost::thread* thread_ao_play_thread;
 		void decoder_read_thread();
