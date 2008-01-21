1659059feae8d37b092131d88610712109d17f92
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-21 18:48:59 +0000

    Fix bug in httpstream causing it to loop the first 64kb.

--- src/audio/datasource_httpstream.h
+++ src/audio/datasource_httpstream.h
@@ -7,15 +7,15 @@
 #include <string>
 
 #define HTTP_STREAM_BUFFER_SIZE 1024*64
-#define HTTP_STREAM_RECV_CHUNK 1024*8
+#define HTTP_STREAM_RECV_CHUNK 1024*32
 
 class HTTPStreamDataSource : public IDataSource {
 	private:
-		uint8 data[HTTP_STREAM_BUFFER_SIZE];
-		uint datarpos;
-		uint datawpos;
-		uint datalen;
-		uint dataofs;
+		uint8 data[HTTP_STREAM_BUFFER_SIZE]; // data buffer
+		uint datarpos;    // read position in buffer
+		uint datawpos;    // write position in buffer
+		uint datalen;     // amount of valid data in buffer
+		uint dataofs;     // current offset to begin of stream of buffer
 		tcp_socket* conn;
 	public:
 		HTTPStreamDataSource();
