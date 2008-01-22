151c3d428a5abed192343ae76a134d6b8b139a00
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-22 12:31:48 +0000

    read vorbis headers && initialize libvorbis with them I think...

--- trunk/src/audio/decoder_vorbis.cpp
+++ trunk/src/audio/decoder_vorbis.cpp
@@ -1,44 +1,99 @@
 #include "decoder_vorbis.h"
 #include "../error-handling.h"
+#include <algorithm>
+using namespace std;
 
 VorbisDecoder::VorbisDecoder() : IDecoder() {
-	packet = NULL;
-	dsp_state = NULL;
-	block = NULL;
-	info = NULL;
-	comment = NULL;
+	dcerr("");
+	datasource = NULL;
 	reset();
 }
 
-void VorbisDecoder::reset() {
-	if(packet) {
-		delete packet->packet;
-		delete packet;
-	}
+VorbisDecoder::VorbisDecoder(IDataSource* ds) : IDecoder() {
+	dcerr(ds);
+	datasource = ds;
+	reset();
+	/* Initialize libvorbis */
+	if(!read_vorbis_headers()) throw "VorbisDecoder: Error while reading vorbis header";
+}
+
+void VorbisDecoder::initialize() {
+	dcerr("");
+	dsp_state = new vorbis_dsp_state();
+	block = new vorbis_block();
+	info = new vorbis_info();
+	comment = new vorbis_comment();
+	vorbis_info_init(info);
+	vorbis_comment_init(comment);
+
 	packet = new ogg_packet();
 	packet->packet = new unsigned char[1<<16]; //Maximum page size = 64k?
 	packet->bytes = 0;
-	packet->b_o_s = 0;
+	packet->b_o_s = 2;
 	packet->e_o_s = 0;
 	packet->granulepos=0;
 	packet->packetno=0;
-
-	if(dsp_state) delete dsp_state;
-	dsp_state = new vorbis_dsp_state();
-
-	if(block) delete block;
-	block = new vorbis_block();
-
-	if(info) delete info;
-	info = new vorbis_info();
-
-	if(comment) delete comment;
-	comment = new vorbis_comment();
 }
 
+void VorbisDecoder::uninitialize() {
+	dcerr("");
+	if(packet) {
+		delete packet->packet;
+		packet->packet = NULL;
+		delete packet;
+		packet=NULL;
+	}
+	if(dsp_state) {
+		delete dsp_state;
+		dsp_state = NULL;
+	}
+	if(block) {
+		delete block;
+		block = NULL;
+	}
+	if(info) {
+		vorbis_info_clear(info);
+		delete info;
+		info = NULL;
+	}
+	if(comment) {
+		vorbis_comment_clear(comment);
+		delete comment;
+		comment = NULL;
+	}
+}
+
+void VorbisDecoder::reset() {
+	dcerr("");
+	uninitialize();
+	if(datasource) datasource->reset();
+	initialize();
+}
 
 VorbisDecoder::~VorbisDecoder() {
+	dcerr("");
+	uninitialize();
+}
 
+void VorbisDecoder::construct_next_packet() {
+	packet->bytes = datasource->read(packet->packet, 1<<16);
+	packet->b_o_s = max(--(packet->b_o_s), (long int)0);
+	packet->e_o_s = datasource->exhausted();
+	packet->granulepos++;
+	packet->packetno++;
+}
+
+bool VorbisDecoder::read_vorbis_headers() {
+	for(int i=0; i<3; ++i) {
+		construct_next_packet();
+		//FIXME: Find out what vorbis_synthesis_headerin returns
+		int result = vorbis_synthesis_headerin(info, comment, packet);
+		if(result<0) {
+			dcerr("vorbis_synthesis_headerin error #" << result << " in header packet "<<i);
+			return false;
+		}
+	}
+	return true;
 }
 
 uint32 VorbisDecoder::doDecode(uint8* buf, uint32 max, uint32 req) {
@@ -47,41 +102,10 @@ uint32 VorbisDecoder::doDecode(uint8* buf, uint32 max, uint32 req) {
 
 IDecoder* VorbisDecoder::tryDecode(IDataSource* ds) {
 	dcerr("");
+	datasource=ds;
 	reset();
-	ds->reset();
 
-	packet->bytes = ds->read(packet->packet, 1<<16);
-	if(!packet->bytes) return NULL;
-
-	vorbis_info_init(info);
-	vorbis_comment_init(comment);
-
-	if(vorbis_synthesis_headerin(info,comment,packet)<0){
-		return NULL;
-	}
-		/* error case; not a vorbis header * /
-		fprintf(stderr,"This Ogg bitstream does not contain Vorbis "
-			"audio data.\n");
-		exit(1);
-	}
-/*
-	if(ogg_stream_pagein(&os,&og)<0){
-		/* error; stream version mismatch perhaps * /
-		fprintf(stderr,"Error reading first page of Ogg bitstream data.\n");
-		exit(1);
-	}
-
-	if(ogg_stream_packetout(&os,&op)!=1){
-		/* no page? must not be vorbis * /
-		fprintf(stderr,"Error reading initial header packet.\n");
-		exit(1);
-	}
-
-	if(vorbis_synthesis_headerin(&vi,&vc,&op)<0){
-		/* error case; not a vorbis header * /
-		fprintf(stderr,"This Ogg bitstream does not contain Vorbis "
-			"audio data.\n");
-		exit(1);
-	}*/
+	if(read_vorbis_headers())
+	return new VorbisDecoder(ds);
 	return NULL;
 }
