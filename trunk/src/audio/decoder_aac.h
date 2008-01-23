4e432529c167fcd74a9b4adc35bc7d9e756a6e99
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-23 15:36:46 +0000

    Yes, an aparantly fully funtioning AAC decoder :D

---
+++ trunk/src/audio/decoder_aac.h
@@ -0,0 +1,38 @@
+#ifndef DECODER_AAC_H
+#define DECODER_AAC_H
+
+#include "decoder_interface.h"
+#include "datasource_interface.h"
+#include <faad.h>
+
+#define BLOCK_SIZE 8*1024
+class AACDecoder : public IDecoder {
+	public:
+		AACDecoder();
+		~AACDecoder();
+		IDecoderRef tryDecode(IDataSourceRef ds);
+		uint32 getData(uint8* buf, uint32 max);
+
+	private:
+		AACDecoder(IDataSourceRef source);
+		IDataSourceRef datasource;
+		AudioFormat audio_format;
+
+		uint32 buffer_fill;
+		uint8 buffer[BLOCK_SIZE];
+		uint32 sample_buffer_size;
+		uint32 sample_buffer_index;
+		uint8* sample_buffer;
+
+		int aac_probe();
+		void fill_buffer();
+
+		uint32 decoder_capabilities;
+		faacDecHandle decoder_handle;
+		faacDecConfigurationPtr decoder_config;
+
+		void uninitialize();
+		void initialize();
+};
+
+#endif//DECODER_AAC_H
