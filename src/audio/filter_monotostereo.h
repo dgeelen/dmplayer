5a702a63e6eba1aea28357c08366a8ca5f6419a5
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-23 19:31:14 +0000

    Implement Mono to Stereo conversion filter.

---
+++ src/audio/filter_monotostereo.h
@@ -0,0 +1,16 @@
+#ifndef FILTER_MONOTOSTEREO_H
+#define FILTER_MONOTOSTEREO_H
+
+#include "audiosource_interface.h"
+
+class MonoToStereoFilter : public IAudioSource {
+	private:
+		IAudioSourceRef src;
+	public:
+		MonoToStereoFilter(IAudioSourceRef as);
+
+		uint32 getData(uint8* buf, uint32 len);
+};
+
+#endif//FILTER_MONOTOSTEREO_H
+
--- src/audio/filter_reformat.cpp
+++ src/audio/filter_reformat.cpp
@@ -4,15 +4,21 @@
 #include <algorithm>
 
 #include "filter_sampledoubler.h"
+#include "filter_monotostereo.h"
 
 using namespace std;
 
 ReformatFilter::ReformatFilter(IAudioSourceRef as, AudioFormat target)
 	: IAudioSource(as->getAudioFormat()), src(as)
 {
+	// mono to stereo if needed
+	if (src->getAudioFormat().Channels == 1 && target.Channels == 2)
+		src = IAudioSourceRef(new MonoToStereoFilter(src));
+
 	// partially fix sample rate (only multiply by powers of 2)
-	//while (src->getAudioFormat().SampleRate < target.SampleRate)
-	//	src = IAudioSourceRef(new SampleDoublerFilter(src));
+	while (src->getAudioFormat().SampleRate < target.SampleRate)
+		src = IAudioSourceRef(new SampleDoublerFilter(src));
+
 
 	audioformat = src->getAudioFormat();
 }
