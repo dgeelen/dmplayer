cba077775b4272620428269da373a8259aa5ec5d
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-23 13:14:45 +0000

    Rework exception handling 'framework'.

---
+++ trunk/src/error-handling.cpp
@@ -0,0 +1,33 @@
+#include "error-handling.h"
+
+using namespace std;
+
+ErrorHandler::ErrorHandler( boost::function<void()> f )
+{
+	this->f = f;
+};
+
+void ErrorHandler::operator()() {
+	try {
+		f();
+	}
+	catch(exception e) {
+		cerr << "---<!ERROR REPORT>---\n";
+		cerr << e.what() << "\n";
+		cerr << "---</ERROR REPORT>---\n";
+		throw;
+	}
+	catch(char const* error_msg) {
+		cerr << "---<!ERROR REPORT>---\n";
+		cerr << error_msg << "\n";
+		cerr << "---</ERROR REPORT>---\n";
+		throw;
+	}
+	catch(...) {
+		cerr << "---<!ERROR REPORT>---\n";
+		cerr << "UNKNOWN ERROR\n";
+		cerr << "---</ERROR REPORT>---\n";
+		throw;
+	}
+};
+
