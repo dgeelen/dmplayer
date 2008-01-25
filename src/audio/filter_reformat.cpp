1cb89108ce12a894438c93f8f1ded59169d72496
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-25 11:28:20 +0000

    fix infinite loop on unset audio format

--- src/audio/filter_reformat.cpp
+++ src/audio/filter_reformat.cpp
@@ -1,6 +1,7 @@
 #include "filter_reformat.h"
 
 #include "../cross-platform.h"
+#include "../error-handling.h"
 #include <algorithm>
 
 #include "filter_sampledoubler.h"
@@ -11,13 +12,23 @@ using namespace std;
 ReformatFilter::ReformatFilter(IAudioSourceRef as, AudioFormat target)
 	: IAudioSource(as->getAudioFormat()), src(as)
 {
+	dcerr("Trying to match audioformats");
+	// Abort on invalid AudioFormat
+	if(src->getAudioFormat().Channels==-1 || target.Channels==-1 ||
+	   src->getAudioFormat().SampleRate==-1 || target.SampleRate==-1 ) {
+		throw Exception("Invalid audioformat!");
+	}
+
 	// mono to stereo if needed
 	if (src->getAudioFormat().Channels == 1 && target.Channels == 2)
 		src = IAudioSourceRef(new MonoToStereoFilter(src));
 
 	// partially fix sample rate (only multiply by powers of 2)
-	while (src->getAudioFormat().SampleRate < target.SampleRate)
+	int count = 0;
+	while ((src->getAudioFormat().SampleRate < target.SampleRate) && (count++<3)) {
+		dcerr(src->getAudioFormat().SampleRate << " < " << target.SampleRate);
 		src = IAudioSourceRef(new SampleDoublerFilter(src));
+	}
 
 
 	audioformat = src->getAudioFormat();
