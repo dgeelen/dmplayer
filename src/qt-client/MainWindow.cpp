7f5870d7db7b10fa49728e3c5c765eca5f7de13f
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-26 00:22:19 +0000

    Make use of STRFORMAT from util/StrFormat.h
    rebase to previous

--- src/qt-client/MainWindow.cpp
+++ src/qt-client/MainWindow.cpp
@@ -2,49 +2,8 @@
 #include "MainWindow.moc"
 #include <QFileDialog>
 
-#include <boost/format.hpp>
+#include "../util/StrFormat.h"
 #include <QString>
-
-/**
- * convenience wrapper for boost::format
- *
- * @param fmt format string
- * @param ... values to fill in
- * @return formatted std::string
- *
- * @see http://www.boost.org/libs/format/doc/format.html
- *      for formatting info etc (its mostly compatible with printf, though)
- */
-
-
-// TODO:put in seperate header
-
-#define STRFORMAT(fmt, ...) \
-	((TStringUtils::detail::macro_str_format(fmt), ## __VA_ARGS__).str())
-
-namespace TStringUtils {
-	namespace detail {
-		// dont allow this class easily into normal namespace
-		// it has dangerous overload for comma operator
-		// so hide it in detail subnamespace
-		class macro_str_format {
-			private:
-				boost::format bfmt;
-				macro_str_format();
-			public:
-				macro_str_format(const std::string & fmt)
-					: bfmt(fmt) {};
-				template<typename T>
-						macro_str_format& operator,(const T & v) {
-					bfmt % v;
-					return *this;
-				}
-				std::string str() {
-					return bfmt.str();
-				}
-		};
-	}
-}
 
 MainWindow::MainWindow()
 {
