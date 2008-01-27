f35e2a5f04bf569c6d17f497b58f780411327d1f
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-27 14:12:57 +0000

    Make decoder vorbis use static tryDecode, fix some memleaks in the process.

--- src/audio/decoder_vorbis.cpp
+++ src/audio/decoder_vorbis.cpp
@@ -3,93 +3,55 @@
 #include <algorithm>
 using namespace std;
 
-VorbisDecoder::VorbisDecoder() : IDecoder(AudioFormat()) {
-	initialize();
-}
-
 VorbisDecoder::VorbisDecoder(IDataSourceRef ds) : IDecoder(AudioFormat()) {
 	datasource = ds;
-	initialize();
 	datasource->reset();
-	/* Initialize libvorbis */
-	if(!read_vorbis_headers()) throw "VorbisDecoder: Error while reading vorbis header";
+
+	vorbis_info_init(&info);
+	onDestroy.add(boost::bind(&vorbis_info_clear, &info));
+
+	vorbis_comment_init(&comment);
+	onDestroy.add(boost::bind(&vorbis_comment_clear, &comment));
+
+	packet.packet = packetbuffer;
+	packet.bytes = 0;
+	packet.b_o_s = 2;
+	packet.e_o_s = 0;
+	packet.granulepos=-1;
+	packet.packetno=-1;
+
+	if (!read_vorbis_headers())
+		throw "VorbisDecoder: Error while reading vorbis header";
+
 	audioformat.LittleEndian = true;
-	audioformat.Channels = info->channels;
+	audioformat.Channels = info.channels;
 	audioformat.BitsPerSample = 16;
-	audioformat.SampleRate = info->rate;
+	audioformat.SampleRate = info.rate;
 	audioformat.SignedSample = true;
-	/* FIXME: These are only used when we have a data source, and unitialized after playing a full track */
-	vorbis_synthesis_init(dsp_state, info);
-	vorbis_block_init(dsp_state, block);
-}
 
-void VorbisDecoder::initialize() {
-	dsp_state = new vorbis_dsp_state();
-	block = new vorbis_block();
-	info = new vorbis_info();
-	comment = new vorbis_comment();
-	vorbis_info_init(info);
-	vorbis_comment_init(comment);
+	vorbis_synthesis_init(&dsp_state, &info);
+	onDestroy.add(boost::bind(&vorbis_dsp_clear, &dsp_state));
 
-	packet = new ogg_packet();
-	packet->packet = new unsigned char[1<<16]; //Maximum page size = 64k?
-	packet->bytes = 0;
-	packet->b_o_s = 2;
-	packet->e_o_s = 0;
-	packet->granulepos=-1;
-	packet->packetno=-1;
-}
-
-void VorbisDecoder::uninitialize() {
-	if(packet) {
-		delete packet->packet;
-		packet->packet = NULL;
-		delete packet;
-		packet=NULL;
-	}
-	if(dsp_state) {
-		delete dsp_state;
-		dsp_state = NULL;
-	}
-	if(block) {
-		delete block;
-		block = NULL;
-	}
-	if(info) {
-		vorbis_info_clear(info);
-		delete info;
-		info = NULL;
-	}
-	if(comment) {
-		vorbis_comment_clear(comment);
-		delete comment;
-		comment = NULL;
-	}
-}
-
-void VorbisDecoder::reset() {
-	uninitialize();
-	if(datasource) datasource->reset();
-	initialize();
+	vorbis_block_init(&dsp_state, &block);
+	onDestroy.add(boost::bind(&vorbis_block_clear, &block));
 }
 
 VorbisDecoder::~VorbisDecoder() {
-	uninitialize();
 }
 
 void VorbisDecoder::construct_next_packet() {
-	packet->bytes = datasource->getData(packet->packet, 1<<16);
-	packet->b_o_s = max(--(packet->b_o_s), (long int)0);
-	packet->e_o_s = (packet->bytes==0) ? 1 : 0;
-	packet->granulepos++;
-	packet->packetno++;
+	packet.bytes = datasource->getData(packet.packet, 1<<16);
+	packet.b_o_s = max(--(packet.b_o_s), (long int)0);
+	packet.e_o_s = (packet.bytes==0) ? 1 : 0;
+	packet.granulepos++;
+	packet.packetno++;
 }
 
 bool VorbisDecoder::read_vorbis_headers() {
 	for(int i=0; i<3; ++i) {
 		construct_next_packet();
 		//FIXME: Find out what vorbis_synthesis_headerin returns
-		int result = vorbis_synthesis_headerin(info, comment, packet);
+		int result = vorbis_synthesis_headerin(&info, &comment, &packet);
 		if(result<0) {
 			dcerr("vorbis_synthesis_headerin error #" << result << " in header packet "<<i);
 			return false;
@@ -97,36 +59,36 @@ bool VorbisDecoder::read_vorbis_headers() {
 	}
 	/* Throw the comments plus a few lines about the bitstream we're decoding */
 	cout << "------< Vorbis Stream Info >------\n"
-	     << "  Vorbis version " << info->version << "\n"
-	     << "  " << info->channels << " channels" << "\n"
-	     << "  " << info->rate << "Hz" << "\n"
-	     << "  bitrate: " << info->bitrate_lower << " < " << info->bitrate_nominal << " < " << info->bitrate_upper << "\n"
-	     << "  bitrate window size " << info->bitrate_window << "\n"
+	     << "  Vorbis version " << info.version << "\n"
+	     << "  " << info.channels << " channels" << "\n"
+	     << "  " << info.rate << "Hz" << "\n"
+	     << "  bitrate: " << info.bitrate_lower << " < " << info.bitrate_nominal << " < " << info.bitrate_upper << "\n"
+	     << "  bitrate window size " << info.bitrate_window << "\n"
 	     << "------< Vorbis Comments    >------\n";
-	char **ptr=comment->user_comments;
+	char **ptr=comment.user_comments;
 	while(*ptr){
 		cout << *ptr << "\n";
 		++ptr;
 	}
-	cout << "Encoded by: " << comment->vendor << "\n"
+	cout << "Encoded by: " << comment.vendor << "\n"
 	     << "------< Vorbis Stream Info >------\n";
 	return true;
 }
 
 uint32 VorbisDecoder::getData(uint8* buffer, uint32 len)
 {
-	const int bytes_per_sample = info->channels * sizeof(ogg_int16_t);
+	const int bytes_per_sample = info.channels * sizeof(ogg_int16_t);
 	const int samples_requested = len / bytes_per_sample;
 	uint32 samples_decoded = 0;
 	bool done = false;
 	while(!done) {
 		float **pcm;
-		int samples=vorbis_synthesis_pcmout(dsp_state, &pcm);
+		int samples=vorbis_synthesis_pcmout(&dsp_state, &pcm);
 		if(samples>0) {
 			int samples_todo = min(samples_requested - samples_decoded, (uint32)samples);
 			bool clipflag=0;
-			for(int channel=0; channel<info->channels; ++channel) { /* convert floats to 16 bit signed ints (host order) and interleave */
-				ogg_int16_t *ptr = ((ogg_int16_t*)buffer) + (samples_decoded*info->channels) + channel;
+			for(int channel=0; channel<info.channels; ++channel) { /* convert floats to 16 bit signed ints (host order) and interleave */
+				ogg_int16_t *ptr = ((ogg_int16_t*)buffer) + (samples_decoded*info.channels) + channel;
 				float *mono_channel=pcm[channel];
 				for(int i=0; i<samples_todo; ++i) {
 					#if 1
@@ -144,19 +106,19 @@ uint32 VorbisDecoder::getData(uint8* buffer, uint32 len)
 						clipflag=true;
 					}
 					*ptr=val;
-					ptr+=info->channels;
+					ptr+=info.channels;
 			  }
 			}
 			samples_decoded+=samples_todo;
 			if(samples_decoded==samples_requested) done = true;
-			if(clipflag) dcerr("Clipping in frame " << (long)(dsp_state->sequence));
-			vorbis_synthesis_read(dsp_state, samples_todo); /* tell libvorbis how many samples we actually consumed */
+			if(clipflag) dcerr("Clipping in frame " << (long)(dsp_state.sequence));
+			vorbis_synthesis_read(&dsp_state, samples_todo); /* tell libvorbis how many samples we actually consumed */
 		}
 		else {
-			if(packet->e_o_s) done=true;
+			if(packet.e_o_s) done=true;
 			construct_next_packet();
-			if(vorbis_synthesis(block, packet)==0) {
-				vorbis_synthesis_blockin(dsp_state, block); //FIXME: Else?
+			if(vorbis_synthesis(&block, &packet)==0) {
+				vorbis_synthesis_blockin(&dsp_state, &block); //FIXME: Else?
 			}
 // 			else dcerr("Warning: could not vorbis_synthesis_blockin()"); //EOF?
 		}
@@ -165,12 +127,12 @@ uint32 VorbisDecoder::getData(uint8* buffer, uint32 len)
 }
 
 IDecoderRef VorbisDecoder::tryDecode(IDataSourceRef ds) {
-	datasource=ds;
-	reset();
-
-	IDecoderRef result;
-	if(read_vorbis_headers())
-		result = IDecoderRef(new VorbisDecoder(ds));
-	datasource.reset(); // don't hang on to the data source longer than needed
-	return result;
+	IDecoderRef decoder;
+	return decoder;
+	try {
+		decoder = IDecoderRef(new VorbisDecoder(ds));
+	} catch (Exception &e) {
+		dcerr("VorbisDecoder::tryDecode failed: " << e.what());
+	}
+	return decoder;
 }
