999e06b09fa4f1cccde7dcfe2134c1074981ca60
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-01-27 14:12:57 +0000

    Make decoder vorbis use static tryDecode, fix some memleaks in the process.

--- trunk/src/audio/decoder_vorbis.h
+++ trunk/src/audio/decoder_vorbis.h
@@ -4,28 +4,28 @@
 #include "decoder_interface.h"
 #include "datasource_interface.h"
 #include <vorbis/codec.h>
+#include "../util/ScopeExitSignal.h"
 
 class VorbisDecoder : public IDecoder {
 	public:
-		VorbisDecoder();
-		VorbisDecoder(IDataSourceRef ds);
 		~VorbisDecoder();
-		void reset();
-		IDecoderRef tryDecode(IDataSourceRef ds);
+		static IDecoderRef tryDecode(IDataSourceRef ds);
 		uint32 getData(uint8* buf, uint32 max);
 	private:
+		VorbisDecoder(IDataSourceRef ds);
 		IDataSourceRef datasource;
-		void initialize();
-		void uninitialize();
 		void construct_next_packet();
 		bool read_vorbis_headers();
-		vorbis_dsp_state* dsp_state;
-		vorbis_block* block;
-		vorbis_info* info;
-		vorbis_comment* comment;
+		vorbis_dsp_state dsp_state;
+		vorbis_block block;
+		vorbis_info info;
+		vorbis_comment comment;
 
 		/* Reconstructing packets */
-		ogg_packet* packet;
+		ogg_packet packet;
+		uint8 packetbuffer[1<<16]; //Maximum page size = 64k?
+
+		ScopeExitSignal onDestroy; // should be last data member
 };
 
 #endif//DECODER_LIBOGG_H
