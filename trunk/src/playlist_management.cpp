e1713e0134c60441ebc709f1ca4c67c2579932fb
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-02-01 16:08:10 +0000

    implement creating trackdb from dir, rough searching in db

--- trunk/src/playlist_management.cpp
+++ trunk/src/playlist_management.cpp
@@ -1,4 +1,9 @@
 #include "playlist_management.h"
+#include "error-handling.h"
+#include "boost/algorithm/string/case_conv.hpp"
+
+namespace fs = boost::filesystem;
+namespace ba = boost::algorithm;
 using namespace std;
 
 /* Class Track */
@@ -31,10 +36,31 @@ void TrackDataBase::add(Track t) {
 	entries.push_back(t);
 }
 
-vector<SearchResult> TrackDataBase::search(Track t) {
-	vector<SearchResult> result;
+vector<Track> TrackDataBase::search(Track t) {
+	vector<Track> result;
+	string filename = ba::to_lower_copy(t.meta_data["FILENAME"]);
 	for(vector<Track>::iterator i = entries.begin(); i!=entries.end(); ++i) {
-		/* Todo: RegEx magix */
+		if( ba::to_lower_copy(i->meta_data["FILENAME"]).find(filename, -1) != -1 ) {
+			result.push_back(*i);
+		}
 	}
 	return result;
 }
+
+void TrackDataBase::add_directory(fs::path path) {
+	if(!fs::exists(path)) return;
+	if(fs::is_directory(path)) {
+		fs::directory_iterator end_iter; // default construction yields past-the-end
+		for(fs::directory_iterator iter(path);  iter != end_iter; ++iter ) {
+			if(fs::exists(iter->path())) {
+				add_directory(iter->path());
+			}
+		}
+	}
+	else { // Some kind of file
+		map<string, string> meta_data;
+		meta_data["FILENAME"] = path.leaf();
+		add(Track(get_first_free_id(), path.string(), meta_data ));
+	}
+}
+
