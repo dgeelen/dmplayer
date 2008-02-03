59363d208cc75822368a97927b71ccc48184b96b
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-02-03 21:42:45 +0000

    hack until bizarre crash-on-close-when-using-or-referencing-'delete_event'-in-any-way-whatsoever bug magically disappeared.... :-S

--- trunk/src/gtk-client/gmpmpc_select_server.cpp
+++ trunk/src/gtk-client/gmpmpc_select_server.cpp
@@ -1,14 +1,3 @@
-//
-// C++ Implementation: gmpmpc_select_server
-//
-// Description:
-//
-//
-// Author:  <>, (C) 2008
-//
-// Copyright: See COPYING file that comes with this distribution
-//
-//
 #include "gmpmpc.h"
 #include "gmpmpc_select_server.h"
 #include "gmpmpc_select_server_window.glade.h"
@@ -20,6 +9,10 @@ using namespace std;
 
 GladeXML *select_server_window = NULL;
 extern network_handler* gmpmpc_network_handler; //FIXME: Global variables == 3vil
+/* Signals */
+int window_select_server_delete_event_signal_id      = 0;
+int button_cancel_server_selection_clicked_signal_id = 0;
+int window_select_server_destroy_signal_id           = 0;
 
 enum {
   SERVER_LIST_COLUMN_NAME,
@@ -44,13 +37,12 @@ void select_server_update_treeview( const vector<server_info>& si) {
   g_object_unref(model);
 */
 
-	dcerr("\n\n---SERVER_LISTING_CHANGED:");
 	try_with_widget(select_server_window, treeview_discovered_servers, tv) {
 		GtkListStore *list_store_servers = GTK_LIST_STORE(gtk_tree_view_get_model(GTK_TREE_VIEW(tv)));
 		gtk_list_store_clear(list_store_servers);
 		GtkTreeIter iter;
 		for(vector<server_info>::const_iterator i = si.begin(); i != si.end(); ++i ) {
-			dcerr(*i << "\n");
+// 			dcerr(*i << "\n");
 			gtk_list_store_append(list_store_servers, &iter);
 			gtk_list_store_set (list_store_servers, &iter,
 													SERVER_LIST_COLUMN_NAME, i->name.c_str(),
@@ -62,16 +54,17 @@ void select_server_update_treeview( const vector<server_info>& si) {
 		gtk_list_store_append(list_store_servers, &iter);
 // 		gtk_list_store_remove(list_store_servers, &iter);
 	}
-	dcerr("+++SERVER_LISTING_CHANGED\n\n");
 }
 
 bool select_server_initialise_window() {
 	/* load the interface from the xml buffer */
 	select_server_window = glade_xml_new_from_buffer(gmpmpc_select_server_window_glade_definition, gmpmpc_select_server_window_glade_definition_size, NULL, NULL);
+
 	if(select_server_window != NULL) {
 		/* Have the delete event (window close) hide the windows and apply setting*/
-		try_connect_signal(select_server_window, window_select_server, delete_event);
-
+ 		try_connect_signal(select_server_window, window_select_server, delete_event);
+		try_connect_signal(select_server_window, window_select_server, destroy);
+		try_connect_signal(select_server_window, button_cancel_server_selection, clicked);
 		/* Initialise store for server treeview with Name, Ping, Last Seen, Address */
 		GtkListStore *list_store_servers = gtk_list_store_new(4, G_TYPE_STRING, G_TYPE_UINT64, G_TYPE_UINT64, G_TYPE_STRING);
 
@@ -118,14 +111,31 @@ bool select_server_initialise_window() {
 	return select_server_window != NULL;
 }
 
-void window_select_server_delete_event(GtkWidget *widget, gpointer user_data) {
+void select_server_uninitialise_window() {
+	if(select_server_window) {
+		disconnect_signal(select_server_window, window_select_server, delete_event);
+		disconnect_signal(select_server_window, button_cancel_server_selection, clicked);
+		disconnect_signal(select_server_window, window_select_server, destroy);
+		GtkWidget* wnd = (GtkWidget*)glade_xml_get_widget(select_server_window, "window_select_server");
+		if(wnd) {
+			gtk_widget_destroy(wnd);
+		}
+	}
+}
+
+void window_select_server_destroy(GtkWidget *widget, gpointer user_data) {
+	select_server_uninitialise_window();
+}
+
+gint window_select_server_delete_event(GtkWidget* widget, GdkEvent* event, gpointer user_data) {
 	if( select_server_window == NULL) {
 		dcerr("window_select_server_delete_event(): Error: select_server_window is NULL!");
-		return;
+		return false; //???
 	}
 	GtkWidget* wnd = (GtkWidget*)glade_xml_get_widget(select_server_window, "window_select_server");
 	gtk_widget_hide_all(wnd);
 	gmpmpc_network_handler->server_list_update_signal.disconnect(select_server_update_treeview);
+	return true;
 }
 
 void imagemenuitem_select_server_activate(GtkWidget *widget, gpointer user_data) {
@@ -137,3 +147,8 @@ void imagemenuitem_select_server_activate(GtkWidget *widget, gpointer user_data)
 	gtk_widget_show_all(wnd);
 	gmpmpc_network_handler->server_list_update_signal.connect(select_server_update_treeview);
 }
+
+void button_cancel_server_selection_clicked(GtkWidget *widget, gpointer user_data) {
+	window_select_server_delete_event(widget, NULL, user_data);
+}
+
