30db1954d053a8f61ba39987a3a5ca0af76d9566
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-02-12 15:59:41 +0000

    Introduce 'extralib' directory which is searched for libraries by cmake.

--- trunk/cmake/FindLibVorbis.cmake
+++ trunk/cmake/FindLibVorbis.cmake
@@ -1,33 +1,41 @@
+find_package(ExtraLibDir)
+
+FIND_LIB_IN_EXTRALIBS(LIBVORBIS *vorbis* include lib)
+
 SET(LIBVORBIS_FOUND FALSE)
 SET(LIBVORBISFILE_FOUND FALSE)
 
 Find_Path(LIBVORBIS_INCLUDE_DIR
-  vorbis/codec.h
-  /usr/include/ /usr/local/include/
-  )
+	vorbis/codec.h
+	/usr/include/ /usr/local/include/
+	${LIBVORBIS_EXTRALIB_INCLUDE_PATHS}
+)
 
 Find_Library(LIBVORBIS_LIBRARY
-  vorbis
-  /usr/lib /usr/local/lib
-  )
+	vorbis
+	/usr/lib /usr/local/lib
+	${LIBVORBIS_EXTRALIB_LIBRARY_PATHS}
+)
 
 IF(LIBVORBIS_INCLUDE_DIR AND LIBVORBIS_LIBRARY)
-  SET(LIBVORBIS_FOUND TRUE)
-  MESSAGE(STATUS "Found vorbis library")
+	SET(LIBVORBIS_FOUND TRUE)
+	MESSAGE(STATUS "Found vorbis library")
 ENDIF(LIBVORBIS_INCLUDE_DIR AND LIBVORBIS_LIBRARY)
 
 Find_Path(LIBVORBISFILE_INCLUDE_DIR
-  vorbis/vorbisfile.h
-  /usr/include/ /usr/local/include/
-  ${LIBVORBIS_INCLUDE_DIR}
-  )
+	vorbis/vorbisfile.h
+	/usr/include/ /usr/local/include/
+	${LIBVORBIS_INCLUDE_DIR}
+	${LIBVORBIS_EXTRALIB_INCLUDE_PATHS}
+)
 
 Find_Library(LIBVORBISFILE_LIBRARY
-  vorbisfile
-  /usr/lib /usr/local/lib
-  )
+	vorbisfile
+	/usr/lib /usr/local/lib
+	${LIBVORBIS_EXTRALIB_LIBRARY_PATHS}
+)
 
 IF(LIBVORBISFILE_INCLUDE_DIR AND LIBVORBISFILE_LIBRARY)
-  SET(LIBVORBISFILE_FOUND TRUE)
-  MESSAGE(STATUS "Found vorbisfile library")
+	SET(LIBVORBISFILE_FOUND TRUE)
+	MESSAGE(STATUS "Found vorbisfile library")
 ENDIF(LIBVORBISFILE_INCLUDE_DIR AND LIBVORBISFILE_LIBRARY)
