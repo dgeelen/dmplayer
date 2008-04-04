ac02fb467fc1355dba83df81a9791ae1b20a7f97
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-04-04 11:22:56 +0000

    fix compilation of libao backend

--- src/audio/backend_libao.cpp
+++ src/audio/backend_libao.cpp
@@ -42,8 +42,8 @@ LibAOBackend::LibAOBackend(AudioController* dec)	: IBackend(dec), fill_buffer_ba
 	try {
 		thread_decoder_read_thread = NULL;
 		thread_ao_play_thread = NULL;
-		thread_decoder_read_thread = new boost::thread(ErrorHandler(boost::bind(&LibAOBackend::decoder_read_thread, this)));
-		thread_ao_play_thread = new boost::thread(ErrorHandler(boost::bind(&LibAOBackend::ao_play_thread, this)));
+		thread_decoder_read_thread = new boost::thread(makeErrorHandler(boost::bind(&LibAOBackend::decoder_read_thread, this)));
+		thread_ao_play_thread = new boost::thread(makeErrorHandler(boost::bind(&LibAOBackend::ao_play_thread, this)));
 	}
 	catch(...) {
 		throw ThreadException("LibAOBackend: Could not start output thread!");
