0e1c34699c15b1256f4b64bf65f469bbe83544aa
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-04-04 11:23:11 +0000

    tmp remove this while new message infrastucture is build

--- trunk/src/gtk-client/gmpmpc_connection_handling.cpp
+++ trunk/src/gtk-client/gmpmpc_connection_handling.cpp
@@ -2,16 +2,16 @@
 #include "../error-handling.h"
 
 void handle_received_message(const message& m) {
-	switch(m.get_message_type()) {
-		case message::MSG_CAPABILITIES: {
-			dcerr("Received a MSG_CAPABILITIES");
-		} break;
-		case message::MSG_DISCONNECT: {
-			dcerr("Received a MSG_DISCONNECT");
-			gmpmpc_network_handler->message_receive_signal.disconnect(handle_received_message);
-		} break;
-		default: {
-			dcerr("Ignoring unknown message");
-		} break;
-	}
+// 	switch(m.get_message_type()) {
+// 		case message::MSG_CAPABILITIES: {
+// 			dcerr("Received a MSG_CAPABILITIES");
+// 		} break;
+// 		case message::MSG_DISCONNECT: {
+// 			dcerr("Received a MSG_DISCONNECT");
+// 			gmpmpc_network_handler->message_receive_signal.disconnect(handle_received_message);
+// 		} break;
+// 		default: {
+// 			dcerr("Ignoring unknown message");
+// 		} break;
+// 	}
 }
