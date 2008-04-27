638e57d647f265cf561a8a9ed78245fcd5813859
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-04-27 11:45:31 +0000

    Support PortAudio v19.

--- src/audio/backend_portaudio.h
+++ src/audio/backend_portaudio.h
@@ -5,9 +5,22 @@
 
 #include "portaudio.h"
 
+// ugh, how ugly is this...
+#ifdef PORT_AUDIO_H
+#define PA_VERSION 18
+#endif
+#ifdef PORTAUDIO_H
+#define PA_VERSION 19
+#endif
+
 class PortAudioBackend : public IBackend{
 	private:
+#if PA_VERSION == 18
 		PortAudioStream *stream;
+#endif
+#if PA_VERSION == 19
+		PaStream *stream;
+#endif
 	public:
 		PortAudioBackend(AudioController* dec);
 		virtual ~PortAudioBackend();
