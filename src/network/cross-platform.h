67bba2fb2ba885ce1ea0fd530b478632172d4b23
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-05-21 08:55:01 +0000

    Generalise network-core to be a pluggable 'library' :-p.

---
+++ src/network/cross-platform.h
@@ -0,0 +1,48 @@
+#ifndef NETWORK_CROSS_PLATFORM_H
+#define NETWORK_CROSS_PLATFORM_H
+
+// TODO: properly figure network stuff out (in conjunction with cmake)
+// ??: http://www.cmake.org/pipermail/cmake-commits/2007-March/001026.html
+#if __WIN32__
+	#define WIN32
+#endif
+
+#ifdef WIN32
+	// defines needed for any windows header we will include
+	#define WIN32_LEAN_AND_MEAN
+	#ifndef NOMINMAX
+		#define NOMINMAX
+	#endif
+
+	// assume winsock on win32 for now
+	#define HAVE_WINSOCK
+#endif
+
+#if defined(HAVE_WINSOCK)
+	#include <Winsock2.h>
+	#include <ws2tcpip.h>
+
+	#define NetGetLastError() WSAGetLastError()
+	#define sipx_family sa_family
+#else
+	#include <netinet/in.h>
+	#include <arpa/inet.h>
+	#include <errno.h> // for errno in NetGetLastError() macro
+	#include <netipx/ipx.h>
+	#include <netdb.h>
+
+	#define closesocket(x) ::close(x)
+	#define NetGetLastError() errno
+
+	typedef int SOCKET;
+
+	#define INVALID_SOCKET  (SOCKET)(~0)
+	#define SOCKET_ERROR            (-1)
+	#define NSPROTO_IPX PF_IPX
+
+	#define sa_nodenum sipx_node
+	#define sa_socket sipx_port
+	#define sa_netnum sipx_network
+#endif
+
+#endif//NETWORK_CROSS_PLATFORM_H
--- src/packet.cpp
+++ src/packet.cpp
@@ -1,8 +1,43 @@
 #include "packet.h"
 using namespace std;
 
+#include "error-handling.h"
+#include <boost/serialization/shared_ptr.hpp>
+
 // NOTE: I'm not sure why it works, but I think these BOOST_CLASS_EXPORTs need to be here...
 BOOST_CLASS_EXPORT(message_connect);
 BOOST_CLASS_EXPORT(message_accept);
 BOOST_CLASS_EXPORT(message_disconnect);
 BOOST_CLASS_EXPORT(message_playlist_update);
+
+void operator<<(tcp_socket& sock, const messagecref msg)
+{
+	stringstream ss;
+	boost::archive::text_oarchive oa(ss);
+	oa << msg;
+	long l = htonl((long)ss.str().size() + 1);
+	sock.send((const uint8*)(&l), sizeof(long));
+	sock.send((const uint8*)ss.str().c_str(), ss.str().size() +1);
+}
+
+void operator>>(tcp_socket& sock,       messageref& msg)
+{
+	try {
+		long l;
+		int rnum;
+		rnum = sock.receive((uint8*)&l, sizeof(long));
+		if (rnum != sizeof(long))
+			throw NetworkException("receive failed");
+		l = ntohl(l);
+		boost::shared_array<uint8> a = boost::shared_array<uint8>(new uint8[l]);
+		rnum = sock.receive(a.get(),l);
+		if (rnum != l)
+			throw NetworkException("receive failed");
+		stringstream ss((char*)a.get());
+		boost::archive::text_iarchive ia(ss);
+		ia >> msg;
+	} catch (NetworkException e) {
+		msg = messageref(new message_disconnect);
+	}
+}
+
