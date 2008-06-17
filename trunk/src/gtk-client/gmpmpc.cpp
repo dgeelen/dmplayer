249dc429c2885cf712e8a29f094a15ca514a5ebc
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-06-17 21:27:19 +0000

    Start implementing Playlist

--- trunk/src/gtk-client/gmpmpc.cpp
+++ trunk/src/gtk-client/gmpmpc.cpp
@@ -1,16 +1,18 @@
+#define __INIT_VARS__
 #include "gmpmpc.h"
 #include "../types.h"
 #include "../network-handler.h"
 #include "../error-handling.h"
-#include "gmpmpc_main_window.glade.h"
 #include "gmpmpc_select_server.h"
 #include "../audio/audio_controller.h"
 #include <boost/program_options.hpp>
+#include "gmpmpc_playlist.h"
 
 using namespace std;
 namespace po = boost::program_options;
 network_handler* gmpmpc_network_handler; //FIXME: Global variables == 3vil
 bool connected_to_server = false;
+TreeviewPlaylist* treeview_playlist;
 
 /* Signals */
 int gmpmpc_main_window_delete_event_signal_id      = 0;
@@ -28,9 +30,6 @@ void gmpmpc_main_window_delete_event(GtkWidget *widget, gpointer user_data) {
 
 int main ( int argc, char *argv[] )
 {
-	gtk_init (&argc, &argv);
-	glade_init();
-
 	int listen_port;
 	bool showhelp;
 	string filename;
@@ -48,23 +47,33 @@ int main ( int argc, char *argv[] )
 		return 1;
 	}
 
-	AudioController ac;
-
 	network_handler nh(listen_port);
+	AudioController ac;
 	gmpmpc_network_handler = &nh;
 
+	gtk_init (&argc, &argv);
+	glade_init();
+
 	main_window = glade_xml_new_from_buffer(gmpmpc_main_window_glade_definition,
-	                                                  gmpmpc_main_window_glade_definition_size,
-	                                                  NULL,
-	                                                  NULL);
+	                                        gmpmpc_main_window_glade_definition_size,
+	                                        NULL,
+	                                        NULL
+	                                       );
 	if(!select_server_initialise_window()) {
 		cerr << "Error while loading server select window!\n";
+	}
+	if(!playlist_initialize()) {
+		cerr << "Error while initializing playlist!\n";
 	}
 
 	/* connect the signals in the interface */
 	try_connect_signal(main_window, imagemenuitem_select_server, activate);
 	try_connect_signal(main_window, gmpmpc_main_window, delete_event);
 	if(!connected_to_server) imagemenuitem_select_server_activate(NULL, NULL);
+	try_with_widget(main_window, treeview_playlist, playlist) {
+		treeview_playlist = new TreeviewPlaylist((GtkTreeView*)playlist);
+	}
+
 	gtk_main ();
 	return 0;
 }
