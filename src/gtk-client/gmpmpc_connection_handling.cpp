ba01774f3bc8cc9aee583d39842fb5d8dda25255
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-06-18 19:27:24 +0000

    implement query trackbd (kinda)

--- src/gtk-client/gmpmpc_connection_handling.cpp
+++ src/gtk-client/gmpmpc_connection_handling.cpp
@@ -1,8 +1,15 @@
 #include "gmpmpc_connection_handling.h"
 #include "gmpmpc_select_server.h"
 #include "gmpmpc_playlist.h"
+#include "gmpmpc_trackdb.h"
+#include <vector>
+#include <glib.h>
+using namespace std;
 
 extern TreeviewPlaylist* treeview_playlist;
+extern TrackDataBase trackDB;
+extern network_handler* gmpmpc_network_handler;
+ClientID gmpmpc_client_id;
 
 void handle_received_message(const messageref m) {
 	switch(m->get_type()) {
@@ -12,6 +19,10 @@ void handle_received_message(const messageref m) {
 		case message::MSG_ACCEPT: {
 			dcerr("Received a MSG_ACCEPT");
 			select_server_accepted();
+			gmpmpc_client_id = boost::static_pointer_cast<message_accept>(m)->cid;
+			gdk_threads_enter();
+			treeview_trackdb_update(NULL);
+			gdk_threads_leave();
 		}; break;
 		case message::MSG_DISCONNECT: {
 			dcerr("Received a MSG_DISCONNECT");
@@ -24,7 +35,15 @@ void handle_received_message(const messageref m) {
 		}; break;
 		case message::MSG_QUERY_TRACKDB: {
 			dcerr("Received a MSG_QUERY_TRACKDB");
-		}; break;
+			message_query_trackdb_ref qr = boost::static_pointer_cast<message_query_trackdb>(m);
+			vector<Track> res;
+			BOOST_FOREACH(LocalTrack t, trackDB.search(qr->search)) {
+				Track tr(TrackID(gmpmpc_client_id, t.id), t.metadata);
+				res.push_back(tr);
+			}
+			message_query_trackdb_result_ref result(new message_query_trackdb_result(qr->qid, res));
+			gmpmpc_network_handler->send_server_message(result);
+			}; break;
 		case message::MSG_QUERY_TRACKDB_RESULT: {
 			dcerr("Received a MSG_QUERY_TRACKDB_RESULT");
 		}; break;
