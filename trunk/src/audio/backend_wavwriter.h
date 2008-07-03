f3d5c5aedcdc32f547f55496c441cd14c1067b89
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-07-03 11:32:24 +0000

    wav writer (for testing)

---
+++ trunk/src/audio/backend_wavwriter.h
@@ -0,0 +1,18 @@
+#ifndef BACKEND_WAVWRITER_H
+#define BACKEND_WAVWRITER_H
+#include "backend_interface.h"
+#include <boost/thread.hpp>
+
+class WAVWriterBackend : public IBackend {
+	public:
+		WAVWriterBackend(AudioController* dec);
+		virtual ~WAVWriterBackend();
+	private:
+		AudioController* decoder;
+		FILE* f;
+		bool done;
+		boost::thread* outputter_thread;
+		void outputter();
+};
+
+#endif//BACKEND_LIBAO_H
