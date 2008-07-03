2876cbdd7f53684ef2d5acd965afa9e688a1258d
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-07-03 11:32:37 +0000

    use callback interface to libsamplerate

--- src/audio/filter_libsamplerate.h
+++ src/audio/filter_libsamplerate.h
@@ -3,17 +3,29 @@
 
 #include "audiosource_interface.h"
 #include <samplerate.h>
+#include <vector>
+#include <boost/bind.hpp>
+#include <boost/function.hpp>
 
 class LibSamplerateFilter : public IAudioSource {
-	private:
-		IAudioSourceRef src;
-		SRC_STATE* samplerate_converter;
-		SRC_DATA   samplerate_converter_state;
 	public:
+		typedef struct CALLBACK_DATA {
+			SRC_STATE *src_state ;
+			uint32 channels;
+			uint32 buffer_begin;
+			uint32 buffer_end;
+			long input_buffer_index;
+			std::vector<float> input_buffer;
+			IAudioSourceRef src;
+		};
+
 		LibSamplerateFilter(IAudioSourceRef as, AudioFormat target);
 		~LibSamplerateFilter();
 		bool exhausted();
 		uint32 getData(uint8* buf, uint32 len);
+	private:
+		IAudioSourceRef src;
+		CALLBACK_DATA callback_data;
 };
 
 #endif //FILTER_LIBSAMPLERATE_H
