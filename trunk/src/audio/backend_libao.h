a86b04acd133e3fbf969f74c45515900607b2875
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2008-12-25 18:49:58 +0000

    Improve AO backend

--- trunk/src/audio/backend_libao.h
+++ trunk/src/audio/backend_libao.h
@@ -6,6 +6,7 @@
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition.hpp>
 #include <boost/thread/barrier.hpp>
+#include <vector>
 
 class LibAOBackend : public IBackend {
 	public:
@@ -17,13 +18,14 @@ class LibAOBackend : public IBackend {
 		ao_device *device;
 		ao_sample_format format;
 		int   playing_buffer;
-		uint8* audio_buffer[2];
+		std::vector<std::vector<uint8> > audio_buffer;
 		volatile bool read_back;
 		volatile bool play_back;
 
 		boost::barrier fill_buffer_barrier;
-		boost::thread* thread_decoder_read_thread;
-		boost::thread* thread_ao_play_thread;
+		boost::mutex   playback_lock_mutex;
+		boost::shared_ptr<boost::thread> thread_decoder_read_thread;
+		boost::shared_ptr<boost::thread> thread_ao_play_thread;
 		void decoder_read_thread();
 		void ao_play_thread();
 
