7749221c3d9b81939c97dc632a274fa0f7770996
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2009-04-29 10:37:49 +0000

    Rewrite libao backend's locking strategy (no more lockups on shutdown!)

--- trunk/src/audio/backend_libao.h
+++ trunk/src/audio/backend_libao.h
@@ -1,36 +1,37 @@
 #ifndef BACKEND_LIBAO_H
 #define BACKEND_LIBAO_H
-#include <ao.h>
 #include "backend_interface.h"
+#include <ao.h>
+#include <vector>
 #include <boost/thread/thread.hpp>
 #include <boost/thread/mutex.hpp>
-#include <boost/thread/condition.hpp>
 #include <boost/thread/barrier.hpp>
-#include <vector>
+#include <boost/thread/condition.hpp>
 
 class LibAOBackend : public IBackend {
 	public:
 		LibAOBackend(AudioController* dec);
 		virtual ~LibAOBackend();
 	private:
-		AudioController* decoder;
-
-		ao_device *device;
-		ao_sample_format format;
-		int   playing_buffer;
-		std::vector<std::vector<uint8> > audio_buffer;
-		volatile bool read_back;
-		volatile bool play_back;
-
-		boost::barrier fill_buffer_barrier;
-		boost::mutex   playback_lock_mutex;
-		boost::shared_ptr<boost::thread> thread_decoder_read_thread;
-		boost::shared_ptr<boost::thread> thread_ao_play_thread;
 		void decoder_read_thread();
 		void ao_play_thread();
-
 		void start_output();
 		void stop_output();
+
+		AudioController* decoder;
+		ao_device *device;
+		ao_sample_format format;
+		std::vector<std::vector<uint8> > audio_buffer;
+
+		bool active;
+		boost::barrier   thread_started_barrier;
+		boost::mutex     readback_lock_mutex;
+		boost::condition read_next_condition;
+		boost::mutex     playback_lock_mutex;
+		boost::condition play_next_condition;
+
+		boost::shared_ptr<boost::thread> thread_decoder_read_thread;
+		boost::shared_ptr<boost::thread> thread_ao_play_thread;
 };
 
 #endif//BACKEND_LIBAO_H
