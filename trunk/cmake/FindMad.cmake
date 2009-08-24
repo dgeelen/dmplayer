20a018fb388c0e6fdc4ff83997d86192e4ab1a12
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2009-08-24 12:02:07 +0000

    CMake update: avoid mixing C-runtimes on windows (*IF* the libraries themselves have been properly compiled...)

--- trunk/cmake/FindMad.cmake
+++ trunk/cmake/FindMad.cmake
@@ -2,7 +2,7 @@ find_package(ExtraLibDir)
 
 FIND_LIB_IN_EXTRALIBS(LIBMAD *mad* include lib)
 
-Find_Path(MAD_INCLUDE_DIR
+Find_Path(LIBMAD_INCLUDE_DIR
 	mad.h
 	usr/include usr/local/include
 	"C:/Program Files/libmad-0.15.1b/"
@@ -10,7 +10,7 @@ Find_Path(MAD_INCLUDE_DIR
 	${LIBMAD_EXTRALIB_INCLUDE_PATHS}
 )
 
-Find_Library(MAD_LIBRARY
+Find_Library(LIBMAD_LIBRARY
 	mad
 	usr/lib usr/local/lib
 	"C:/Program Files/libmad-0.15.1b/.libs"
@@ -18,8 +18,22 @@ Find_Library(MAD_LIBRARY
 	${LIBMAD_EXTRALIB_LIBRARY_PATHS}
 )
 
-SET(MAD_FOUND FALSE)
-IF (MAD_INCLUDE_DIR AND MAD_LIBRARY)
-	SET(MAD_FOUND TRUE)
+Find_Library(LIBMAD_DEBUG_LIBRARY
+	madd
+	usr/lib usr/local/lib
+	"C:/Program Files/libmad-0.15.1b/.libs"
+	"D:/libmad-0.15.1b/.libs"
+	${LIBMAD_EXTRALIB_LIBRARY_PATHS}
+)
+
+IF(NOT MSVC) # Assume non-MSVC compilers don't care about different runtimes
+	SET(LIBMAD_DEBUG_LIBRARY ${LIBMAD_LIBRARY})
+ENDIF(NOT MSVC)
+
+SET(LIBMAD_FOUND FALSE)
+SET(LIBMAD_DEBUG_FOUND FALSE)
+IF(LIBMAD_INCLUDE_DIR AND LIBMAD_LIBRARY AND LIBMAD_DEBUG_LIBRARY)
+	SET(LIBMAD_FOUND TRUE)
+	SET(LIBMAD_DEBUG_FOUND TRUE)
 	MESSAGE(STATUS "Found Mad library")
-ENDIF (MAD_INCLUDE_DIR AND MAD_LIBRARY)
+ENDIF(LIBMAD_INCLUDE_DIR AND LIBMAD_LIBRARY AND LIBMAD_DEBUG_LIBRARY)
