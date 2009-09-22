94bd464dd4949dfc3e2ff76aacaa9573b96a2c8f
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2009-09-22 11:09:48 +0000

    implement a manual connect tab for gmpmpc (boo wireless!...)

--- src/gtk-client/gmpmpc_select_server.cpp
+++ src/gtk-client/gmpmpc_select_server.cpp
@@ -10,25 +10,45 @@ gmpmpc_select_server_window::gmpmpc_select_server_window(middle_end& _middleend)
 : middleend(_middleend)
 {
 	set_title( "Select a server" );
-	set_default_size( 512, 320);
+	set_default_size( 320, 240);
 	set_position( Gtk::WIN_POS_CENTER_ON_PARENT );
-	framebox.add(serverlist);
+
+	scroll.add(serverlist);
+	scroll.set_policy(Gtk::POLICY_NEVER, Gtk::POLICY_AUTOMATIC);
+	selection_type_notebook.append_page(scroll, "Server list");
+	manual_ip_label.set_text("Please enter the IPv4 address (xxx.xxx.xxx.xxx, with 0 <= xxx <= 255) of the server you wish to connect to.");
+	manual_ip_label.set_line_wrap(true);
+	manual_ip_label.set_size_request(320,-1);
+	manual_ip_vbox.set_border_width(32);
+	manual_ip_vbox.add(manual_ip_label);
+	manual_ip_entry.set_size_request(320,-1);
+	manual_ip_entry.set_max_length(3+1+3+1+3+1+3);
+ 	manual_ip_entry.set_width_chars(3+1+3+1+3+1+3+1);
+	manual_ip_entry.set_activates_default(true);
+	manual_ip_vbox.add(manual_ip_entry);
+	selection_type_notebook.append_page(manual_ip_vbox, "Manual connect");
+	#ifdef DEBUG
+	manual_ip_entry.set_text("127.0.0.1");
+	selection_type_notebook.set_current_page(1);
+	#else
+	manual_ip_entry.set_text("131.155.99.208");
+	#endif
+	vbox.add(selection_type_notebook);
+	cancel_button.set_label("Cancel");
 	hbox.add(cancel_button);
+	connect_button.set_label("Connect");
 	hbox.add(connect_button);
-	framebox.pack_start(hbox, Gtk::PACK_SHRINK);
-	frame.add(framebox);
-	vbox.add(frame);
+	vbox.pack_start(hbox, Gtk::PACK_SHRINK);
 	vbox.pack_start(statusbar, Gtk::PACK_SHRINK);
 	add(vbox);
-
-	connect_button.set_label("Connect");
-	cancel_button.set_label("Cancel");
-	frame.set_label("Servers:");
+	connect_button.set_flags(Gtk::CAN_DEFAULT); // NOTE: set_can_default(true) does not work? (is not a member of ...)
+	set_default(connect_button);
 
 	cancel_button.signal_clicked().connect(boost::bind(&gmpmpc_select_server_window::on_cancel_button_click, this));
 	connect_button.signal_clicked().connect(boost::bind(&gmpmpc_select_server_window::on_connect_button_click, this));
-	signal_window_state_event().connect(sigc::mem_fun(*this, &gmpmpc_select_server_window::focus_connect_button));
-	focus_connect_button(NULL);
+	selection_type_notebook.signal_switch_page().connect(boost::bind(&gmpmpc_select_server_window::on_selection_type_notebook_switch_page, this, _1, _2));
+	signal_window_state_event().connect(sigc::mem_fun(*this, &gmpmpc_select_server_window::update_focus));
+	connect_button.grab_focus();
 
 	serverlist.add_events(Gdk::BUTTON_PRESS_MASK);
 	serverlist.signal_button_press_event().connect(sigc::mem_fun(*this, &gmpmpc_select_server_window::on_serverlist_double_click), false);
@@ -64,8 +84,17 @@ gmpmpc_select_server_window::~gmpmpc_select_server_window() {
 	sig_connect_to_server_failure_connection.disconnect();
 }
 
-bool gmpmpc_select_server_window::focus_connect_button(GdkEventWindowState* event) {
-	connect_button.grab_focus();
+void gmpmpc_select_server_window::on_selection_type_notebook_switch_page(GtkNotebookPage* page, guint page_num) {
+	if(page_num == 1) 
+		manual_ip_entry.grab_focus();
+}
+
+
+bool gmpmpc_select_server_window::update_focus(GdkEventWindowState* event) {
+	if(selection_type_notebook.get_current_page() == 0)
+		serverlist.grab_focus();
+	else
+		manual_ip_entry.grab_focus();
 	return false;
 }
 
@@ -137,31 +166,47 @@ bool gmpmpc_select_server_window::on_serverlist_double_click(GdkEventButton *eve
 	if (event->type == GDK_2BUTTON_PRESS && event->button == 1) {
 		on_connect_button_click();
 	}
-	Glib::signal_idle().connect(sigc::bind(sigc::mem_fun(*this, &gmpmpc_select_server_window::focus_connect_button), (GdkEventWindowState*)NULL));
+	Glib::signal_idle().connect(sigc::bind(sigc::mem_fun(*this, &gmpmpc_select_server_window::update_focus), (GdkEventWindowState*)NULL));
 	return false;
 }
 
 void gmpmpc_select_server_window::on_connect_button_click() {
-	Glib::RefPtr<Gtk::TreeModel> model = serverlist.get_model();
-	Glib::RefPtr<Gtk::ListStore> store = Glib::RefPtr<Gtk::ListStore>::cast_dynamic(model);
+	target_server = ipv4_socket_addr();
+	if(selection_type_notebook.get_current_page() == 0) {
+		Glib::RefPtr<Gtk::TreeModel> model = serverlist.get_model();
+		Glib::RefPtr<Gtk::ListStore> store = Glib::RefPtr<Gtk::ListStore>::cast_dynamic(model);
 
-	Glib::RefPtr<Gtk::TreeSelection> sel = serverlist.get_selection();
-	if(sel->count_selected_rows() == 1) {
+		Glib::RefPtr<Gtk::TreeSelection> sel = serverlist.get_selection();
+		if(sel->count_selected_rows() == 1) {
+			target_server = (*sel->get_selected())[m_Columns.addr];
+		}
+	}
+	else { // page 1
+		ipv4_addr addr;
+		Glib::ustring ipv4addr_str(manual_ip_entry.get_text());
+		ipv4addr_str += '.';
+		int i = 0;
+		while(!ipv4addr_str.empty() && i < 4) {
+			size_t ppos = ipv4addr_str.find_first_of('.');
+			Glib::ustring nstr = ipv4addr_str.substr(0, ppos);
+			ipv4addr_str.erase(0, ppos+1);
+			addr.array[i] = atoi(nstr.c_str()); // FIXME: eeeewww...
+			++i;
+		}
+		target_server = ipv4_socket_addr(addr, TCP_PORT_NUMBER);
+	}
+	if(target_server != ipv4_socket_addr()) {
 		connect_button.set_sensitive(false);
 		serverlist.set_sensitive(false);
-
+		manual_ip_entry.set_sensitive(false);
 		statusbar.pop();
 		statusbar.push("Connecting...");
 		status_message_signal("Connecting...");
-		target_server = (*sel->get_selected())[m_Columns.addr];
-// 		target_server.first.array[3] = 32;
-// 		sig_connect_to_server(target_server);
 		middleend.connect_to_server(target_server, 0);
 	}
 }
 
 void gmpmpc_select_server_window::on_cancel_button_click() {
-
 	if(connect_button.sensitive()) {
 		hide();
 	}
@@ -169,6 +214,7 @@ void gmpmpc_select_server_window::on_cancel_button_click() {
 		middleend.cancel_connect_to_server(target_server);
 		connect_button.set_sensitive(true);
 		serverlist.set_sensitive(true);
+		manual_ip_entry.set_sensitive(true);
 	}
 	statusbar.pop();
 }
@@ -180,6 +226,7 @@ void gmpmpc_select_server_window::connection_denied(ipv4_socket_addr addr, std::
 		dialog.run();
 		connect_button.set_sensitive(true);
 		serverlist.set_sensitive(true);
+		manual_ip_entry.set_sensitive(true);
 		statusbar.pop();
 	}
 }
@@ -188,6 +235,7 @@ void gmpmpc_select_server_window::connection_accepted(ipv4_socket_addr addr, Cli
 	if(addr == target_server) {
 		connect_button.set_sensitive(true);
 		serverlist.set_sensitive(true);
+		manual_ip_entry.set_sensitive(true);
 		statusbar.pop();
 		Glib::RefPtr<Gtk::TreeSelection> sel = serverlist.get_selection();
 		if(sel->count_selected_rows() == 1) {
--- src/gtk-client/gmpmpc_select_server.h
+++ src/gtk-client/gmpmpc_select_server.h
@@ -10,6 +10,9 @@
 	#include <gtkmm/treeview.h>
 	#include <gtkmm/frame.h>
 	#include <gtkmm/button.h>
+	#include <gtkmm/notebook.h>
+	#include <gtkmm/entry.h>
+	#include <gtkmm/label.h>
 	#include "../network-handler.h"
 	#include "../middle_end.h"
 	#include "dispatcher_marshaller.h"
@@ -19,14 +22,6 @@
 		public:
 			gmpmpc_select_server_window(middle_end& _middleend);
 			~gmpmpc_select_server_window();
-// 			void update_serverlist(const std::vector<server_info>& si);
-			void add_servers(const std::vector<server_info>& si);
-			void remove_servers(const std::vector<server_info>& si);
-
-			void connection_accepted(ipv4_socket_addr addr, ClientID id);
-			void connection_denied(ipv4_socket_addr addr, std::string reason);
-			boost::signal<void(ipv4_socket_addr)> sig_connect_to_server;
-			boost::signal<void(ipv4_socket_addr)> sig_cancel_connect_to_server;
 			boost::signal<void(std::string)>      status_message_signal;
 
 		private:
@@ -35,6 +30,7 @@
 			boost::signals::connection sig_connect_to_server_success_connection;
 			boost::signals::connection sig_servers_added_connection;
 			boost::signals::connection sig_servers_removed_connection;
+			boost::signals::connection sig_selection_type_notebook_switch_page_connection;
 			DispatcherMarshaller dispatcher; // Execute a function in the gui thread
 			ipv4_socket_addr target_server;
 			
@@ -56,19 +52,26 @@
 
 			void save_selected();
 			void restore_selected();
+			void connection_accepted(ipv4_socket_addr addr, ClientID id);
+			void connection_denied(ipv4_socket_addr addr, std::string reason);
+			void add_servers(const std::vector<server_info>& si);
+			void remove_servers(const std::vector<server_info>& si);
 			ipv4_socket_addr selected_addr;
 			void on_cancel_button_click();
 			void on_connect_button_click();
 			bool on_serverlist_double_click(GdkEventButton *event);
-			bool focus_connect_button(GdkEventWindowState* event); // A tad hacky, but it saves us an extra function doing the same...
-			Gtk::Frame          frame;
-			Gtk::VBox           framebox;
-			Gtk::VBox           vbox;
-			Gtk::HBox           hbox;
-			Gtk::ScrolledWindow scroll;
+			void gmpmpc_select_server_window::on_selection_type_notebook_switch_page(GtkNotebookPage* page, guint page_num);
+			bool update_focus(GdkEventWindowState* event);
+			Gtk::Entry          manual_ip_entry;
 			Gtk::TreeView       serverlist;
 			Gtk::Button         connect_button;
 			Gtk::Button         cancel_button;
 			Gtk::Statusbar      statusbar;
+			Gtk::VBox           vbox;
+			Gtk::HBox           hbox;
+			Gtk::VBox           manual_ip_vbox;
+			Gtk::Notebook       selection_type_notebook;
+			Gtk::Label          manual_ip_label;
+			Gtk::ScrolledWindow scroll;
 	};
 #endif // GMPMPC_SELECT_SERVER_H
