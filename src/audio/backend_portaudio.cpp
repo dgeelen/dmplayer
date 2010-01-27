39f324ab9216da3c04381cd51182b8d4485fead2
daniel.geelen <daniel.geelen@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2010-01-27 09:55:24 +0000

    update portaudio backend to support multi channel output. Remove V18 stuff.

--- src/audio/backend_portaudio.cpp
+++ src/audio/backend_portaudio.cpp
@@ -2,83 +2,107 @@
 #include "../error-handling.h"
 #include "../util/StrFormat.h"
 
-#define SAMPLE_RATE    (44100)
-#define FRAMES_PER_BUFFER (1024*4)  /* number of frames(=samples) per buffer */
-#define NUM_BUFFERS        (2)  /* number of buffers, if zero then use default minimum */
+using namespace std;
 
-#if PA_VERSION == 18
-static int pa_callback( void *inputBuffer, void *outputBuffer,
-                        unsigned long frameCount,
-                        PaTimestamp outTime, void *userData )
-#endif
-#if PA_VERSION == 19
-static int pa_callback(const void *inputBuffer, void *outputBuffer,
+int PortAudioBackend::pa_callback(const void *inputBuffer, void *outputBuffer,
                        unsigned long frameCount,
                        const PaStreamCallbackTimeInfo* timeInfo,
                        PaStreamCallbackFlags statusFlags,
                        void *userData )
-#endif
 {	// FIXME: This callback runs with PRIO_REALTIME, and can potentially
 	//        halt the system if data is not provided at an adequate pace.
 	//        (such as happens for example at the end of the getData()
 	//         chain, which is connected to the network...)
-	AudioController* ac = (AudioController*)userData;
+	PortAudioBackend* be = (PortAudioBackend*)userData;
 	char *out = (char*)outputBuffer;
 
-	// 2 byte samples + 2 channels -> 2*2=4 -> 4 bytes/frame
-	uint32 act = ac->getData((uint8*)out, frameCount*4);
+	// Float32 is 4 bytes, so #channels * 4 = bytes/frame
+	uint32 act = be->dec->getData((uint8*)out, frameCount*(be->af.Channels)*4);
 	return 0;
 }
 
-PortAudioBackend::PortAudioBackend(AudioController* dec)
-	: IBackend(dec)
+PortAudioBackend::PortAudioBackend(AudioController* _dec)
+	: IBackend(dec), dec(_dec)
 {
 	PaError err = Pa_Initialize();
-	if (err != paNoError) throw SoundException("PortAudioBackend initialisation failed");
+	if (err != paNoError) throw SoundException("PortAudio backend: ERROR: initialisation failed");
 
-	af.SampleRate = SAMPLE_RATE;
-	af.Channels = 2;
-	af.BitsPerSample = 16;
-	af.SignedSample = true;
-	af.LittleEndian = true;
+	int numDevices = Pa_GetDeviceCount();
+	if(numDevices < 0) {
+		throw SoundException(STRFORMAT("PortAudio backend: ERROR: Pa_GetDeviceCount returned 0x%x (%s)\n", numDevices, Pa_GetErrorText(numDevices)));
+	}
+	if(numDevices == 0) {
+		throw SoundException("PortAudio backend: ERROR: No output devices available");
+	}
+	const   PaDeviceInfo *deviceInfo;
+	int default_device = Pa_GetDefaultOutputDevice();
+	deviceInfo = Pa_GetDeviceInfo( default_device );
+	int best_device    = default_device;
+	int n_channels     = deviceInfo->maxOutputChannels;
+	double samplerate  = deviceInfo->defaultSampleRate;
+	double latency     = deviceInfo->defaultLowOutputLatency;
+	for(int i=0; i<numDevices; i++) {
+		deviceInfo = Pa_GetDeviceInfo( i );
+		dcerr( "Found device #" << i << ": '" << deviceInfo->name << "'");
+		dcerr( "\tmaxInputChannels=" << deviceInfo->maxInputChannels);
+		dcerr( "\tmaxOutputChannels=" << deviceInfo->maxOutputChannels);
+		dcerr( "\tdefaultSampleRate=" << deviceInfo->defaultSampleRate);
 
-#if PA_VERSION == 18
-	err = Pa_OpenStream(
-		&stream,
-		paNoDevice,        /* no input device */
-		0,                 /* no input */
-		paInt16,           /* 16 bit signed input */
-		NULL,
-		Pa_GetDefaultOutputDeviceID(),
-		2,                 /* stereo output */
-		paInt16,           /* 16 bit signed output */
-		NULL,
-		SAMPLE_RATE,
-		FRAMES_PER_BUFFER,
-		NUM_BUFFERS,
-		paClipOff,         /* we won't output out of range samples so don't bother clipping them */
-		pa_callback,
-		dec );
-#endif
-#if PA_VERSION == 19
-	PaStreamParameters outparams;
-	outparams.channelCount = 2;
-	outparams.device = Pa_GetDefaultOutputDevice();
-	outparams.sampleFormat = paInt16;
-	outparams.hostApiSpecificStreamInfo = NULL;
-	outparams.suggestedLatency = 0.2;
-	err = Pa_OpenStream(
-		&stream,
-		NULL,
-		&outparams,
-		SAMPLE_RATE,
-		FRAMES_PER_BUFFER,
-		0,
-		pa_callback,
-		dec );
-#endif
+		PaStreamParameters outputParameters;
+		outputParameters.channelCount = deviceInfo->maxOutputChannels;
+		outputParameters.device = i;
+		outputParameters.hostApiSpecificStreamInfo = NULL;
+		outputParameters.sampleFormat = paFloat32;
+		outputParameters.suggestedLatency = deviceInfo->defaultLowOutputLatency;
+		if(Pa_IsFormatSupported(NULL, &outputParameters, deviceInfo->defaultSampleRate) == paFormatIsSupported) {
+			if( ((deviceInfo->maxOutputChannels > n_channels) &&  (deviceInfo->defaultSampleRate >= samplerate)))
+			{
+				best_device = i;
+				n_channels = deviceInfo->maxOutputChannels;
+				samplerate = deviceInfo->defaultSampleRate;
+				latency    = deviceInfo->defaultLowOutputLatency;
+			}
+			dcerr("device supports " << deviceInfo->maxOutputChannels << " channels, " << deviceInfo->defaultSampleRate << "Hz, Float32 output, with " << deviceInfo->defaultLowOutputLatency << "s latency");
+		}
+		else {
+			dcerr("device does not support " << deviceInfo->maxOutputChannels << " channels, " << deviceInfo->defaultSampleRate << "Hz, Float32 output, with " << deviceInfo->defaultLowOutputLatency << "s latency");
+		}
+	}
+	dcerr("Selecting device #" << best_device << " (default is #" << default_device << ")");
+	
+	if(fabs(samplerate - double(long(samplerate))) > 0.5)
+		cout << "PortAudio backend: WARNING: samplerate mismatch > 0.5!" << endl;
 
-	if (err != paNoError) throw SoundException("failed to open portaudio stream");
+	af.SampleRate    = uint32(samplerate);
+	af.Channels      = n_channels > 6 ? 6 : 2;
+	af.BitsPerSample = 32;
+	af.SignedSample  = true;
+	af.LittleEndian  = true;
+	af.Float         = true;
+
+	PaStreamParameters outputParameters;
+	outputParameters.channelCount              = af.Channels;
+	outputParameters.device                    = best_device;
+	outputParameters.sampleFormat              = paFloat32;
+	outputParameters.hostApiSpecificStreamInfo = NULL;
+	outputParameters.suggestedLatency          = latency;
+
+	if(Pa_IsFormatSupported(NULL, &outputParameters, samplerate) == paFormatIsSupported) {
+		if(Pa_OpenStream(
+			&stream,
+			NULL,
+			&outputParameters,
+			samplerate,
+			1024 * outputParameters.channelCount * 4, /* Float32 is 4 bytes */
+			paClipOff,         /* we won't output out of range samples so don't bother clipping them */
+			pa_callback,
+			(void*)this)
+		!= paNoError)
+			throw SoundException("PortAudio backend: ERROR: failed to open portaudio stream");
+	}
+	else {
+		throw SoundException("PortAudio backend: ERROR: Format not supported!");
+	}
 };
 
 PortAudioBackend::~PortAudioBackend()
