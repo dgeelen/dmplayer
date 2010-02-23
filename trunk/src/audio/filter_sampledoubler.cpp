f92fc5622c84cdc109db7a313eec2890307e3990
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2010-02-23 16:27:51 +0000

    Fix problem with SampleDoublerFilter.

--- trunk/src/audio/filter_sampledoubler.cpp
+++ trunk/src/audio/filter_sampledoubler.cpp
@@ -8,6 +8,7 @@ using namespace std;
 SampleDoublerFilter::SampleDoublerFilter(IAudioSourceRef as)
 	: IAudioSource(as->getAudioFormat()), src(as)
 {
+	if (audioformat.BitsPerSample * audioformat.Channels != 32) throw std::runtime_error("Works only with 32 bits per frame");
 	audioformat.SampleRate *= 2;
 }
 
@@ -16,7 +17,7 @@ uint32 SampleDoublerFilter::getData(uint8* buf, uint32 len)
 	uint8 buffer[1024];
 	uint32 res = 0;
 	while (res < len) {
-		int read = min(len>>1, (uint32)1024);
+		int read = min((len-res)>>1, (uint32)1024);
 		read = src->getData(buffer, read);
 		if (!read) break;
 		for (int i = 0; i < read; ) {
