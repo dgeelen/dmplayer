c5f5be55ac7d9debd9f21815413e67ed0432f0ec
simon.sasburg <simon.sasburg@e69229e2-1541-0410-bedc-87cb2d1b0d4b>
2010-02-25 15:23:03 +0000

    Implement new decoder plugin interface supporting the use of IDecoder::findDecoder in plugins.

---
+++ src/audio/decoder_plugin_legacy.cpp
@@ -0,0 +1,89 @@
+#include "decoder_plugin_legacy.h"
+
+// Legacy Stuff for Decoder-1 plugin type
+namespace {
+	uint32 source_callback_getData(void* source, uint8* buf, uint32 max)
+	{
+		return ((IDataSource*)source)->getData(buf, max);
+	}
+
+	void source_callback_reset(void* source)
+	{
+		((IDataSource*)source)->reset();
+	}
+
+	bool source_callback_exhausted(void* source)
+	{
+		return ((IDataSource*)source)->exhausted();
+	}
+
+	uint32 source_callback_getpos(void* source)
+	{
+		return ((IDataSource*)source)->getpos();
+	}
+
+	void decoder_destroy(ppvoid_t obj)
+	{
+		typedef void (*cbtype)(void*);
+		((cbtype)obj[5])(obj[0]);
+		delete obj;
+	}
+
+	uint32 decoder_getdata(ppvoid_t obj, uint8* buf, uint32 max)
+	{
+		try {
+			if (max == 0xffffffff) --max;
+			typedef uint32 (*cbtype)(void*, uint8*, uint32);
+			return ((cbtype)obj[6])(obj[0], buf, max);
+		} catch (...) {
+			return 0xffffffff;
+		}
+	}
+
+	bool decoder_exhausted(ppvoid_t obj)
+	{
+		typedef bool (*cbtype)(void*);
+		return ((cbtype)obj[7])(obj[0]);
+	}
+
+	void decoder_getaudioformat(ppvoid_t obj, uint32* a, uint32* b, uint32* c, bool* d, bool* e, bool* f)
+	{
+		typedef bool (*cbtype)(void*, uint32*, uint32*, uint32*, bool*, bool*, bool*);
+		((cbtype)obj[8])(obj[0], a, b, c, d, e, f);
+	}
+}
+
+ppvoid_t decoder_plugin_legacy_1(DynamicLibrary& dll, IDataSourceRef source)
+{
+	ppvoid_t decoder_obj = NULL;
+
+	typedef uint32 (*source_callback_getdata_cbt)(void*, uint8*, uint32);
+	typedef void (*source_callback_reset_cbt)(void*);
+	typedef bool (*source_callback_exhausted_cbt)(void*);
+	typedef uint32 (*source_callback_getpos_cbt)(void*);
+
+	boost::function<void*(void*,source_callback_getdata_cbt,source_callback_reset_cbt,source_callback_exhausted_cbt,source_callback_getpos_cbt)> dll_create;
+
+	if (!dll.getFunction("create", dll_create)
+	 || !dll.getFunction("destroy")
+	 || !dll.getFunction("getdata")
+	 || !dll.getFunction("exhausted")
+	 || !dll.getFunction("getaudioformat")
+	 ) throw std::runtime_error("Decoder-1: Failed to load functions");
+
+	void* dlldecoder = dll_create(source.get(), &source_callback_getData, &source_callback_reset, &source_callback_exhausted, &source_callback_getpos);
+	if (!dlldecoder) throw std::runtime_error("Decoder-1: Failed to create plugin decoder");
+
+	decoder_obj = new void*[9];
+	decoder_obj[0] = (void*)dlldecoder;
+	decoder_obj[1] = (void*)&decoder_destroy;
+	decoder_obj[2] = (void*)&decoder_getdata;
+	decoder_obj[3] = (void*)&decoder_exhausted;
+	decoder_obj[4] = (void*)&decoder_getaudioformat;
+	decoder_obj[5] = (void*)dll.getFunction("destroy");
+	decoder_obj[6] = (void*)dll.getFunction("getdata");
+	decoder_obj[7] = (void*)dll.getFunction("exhausted");
+	decoder_obj[8] = (void*)dll.getFunction("getaudioformat");
+
+	return decoder_obj;
+}
--- src/audio/plugin_helper.cpp
+++ src/audio/plugin_helper.cpp
@@ -7,90 +7,32 @@
 #include <boost/shared_ptr.hpp>
 
 namespace {
-	class WrappedDataSource: public IDataSource {
-		public:
-			void* source;
-			source_callback_getData_cbt cb_getdata;
-			source_callback_reset_cbt cb_reset;
-			source_callback_exhausted_cbt cb_exhausted;
-			source_callback_getpos_cbt cb_getpos;
-
-		public:
-			virtual uint32 getpos() {
-				return cb_getpos(source);
-			}
-
-			virtual bool exhausted()
-			{
-				return cb_exhausted(source);
-			}
-
-			virtual void reset()
-			{
-				cb_reset(source);
-			}
-
-			virtual uint32 getData(uint8* buffer, uint32 len)
-			{
-				return cb_getdata(source,buffer,len);
-			}
-	};
-
-	struct WrappedDecoder {
-		IDecoderRef impl;
-	};
-}
+	find_decoder_callback_type find_decoder_callback = NULL;
+};
 
 extern "C" const char* getplugintype()
 {
-	return "Decoder-1";
+	return "Decoder-2";
 }
 
-extern "C" DLL_PUBLIC void* create(void* s,source_callback_getData_cbt cb1,source_callback_reset_cbt cb2,source_callback_exhausted_cbt cb3,source_callback_getpos_cbt cb4)
+extern "C" DLL_PUBLIC ppvoid_t create_decoder(ppvoid_t dsobj)
 {
-	boost::shared_ptr<WrappedDataSource> ds(new WrappedDataSource());
-	ds->source = s;
-	ds->cb_getdata = cb1;
-	ds->cb_reset = cb2;
-	ds->cb_exhausted = cb3;
-	ds->cb_getpos = cb4;
-
-	ds->reset();
+	IDataSourceRef ds = PPVoidToIDataSource(dsobj);
 	IDecoderRef dec = DECODER_CLASS::tryDecode(ds);
-
 	if (!dec) return NULL;
-
-	WrappedDecoder* wrapper = new WrappedDecoder();
-	wrapper->impl = dec;
-
-	return wrapper;
+	return IDecoderToPPVoid(dec);
 }
 
-extern "C" void destroy(void* w)
+extern "C" DLL_PUBLIC void set_find_decoder_callback(find_decoder_callback_type cb)
 {
-	delete (WrappedDecoder*)w;
+	if (!find_decoder_callback)
+		find_decoder_callback = cb; // assignment of function pointer assumed atomic :O
 }
 
-extern "C" uint32 getdata(void* w,uint8* buf,uint32 max)
-{
-	try {
-		return ((WrappedDecoder*)w)->impl->getData(buf, max);
-	} catch (...) {
-		return 0xffffffff;
-	}
-}
 
-extern "C" bool exhausted(void* w)
+IDecoderRef IDecoder::findDecoder(IDataSourceRef ds)
 {
-	return ((WrappedDecoder*)w)->impl->exhausted();
-}
-
-extern "C" DLL_PUBLIC void getaudioformat(void* w, uint32* a,uint32* b,uint32* c,bool* d,bool* e,bool* f)
-{
-	*a = ((WrappedDecoder*)w)->impl->getAudioFormat().SampleRate;
-	*b = ((WrappedDecoder*)w)->impl->getAudioFormat().Channels;
-	*c = ((WrappedDecoder*)w)->impl->getAudioFormat().BitsPerSample;
-	*d = ((WrappedDecoder*)w)->impl->getAudioFormat().SignedSample;
-	*e = ((WrappedDecoder*)w)->impl->getAudioFormat().LittleEndian;
-	*f = ((WrappedDecoder*)w)->impl->getAudioFormat().Float;
+	if (!find_decoder_callback)
+		return IDecoderRef();
+	return PPVoidToIDecoder(find_decoder_callback(IDataSourceToPPVoid(ds)));
 }
